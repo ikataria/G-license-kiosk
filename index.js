2a718e9612e9721ac66ad48850779c5756dc5dd0
iKataria <17ktaria@gmail.com>
2022-10-12 23:35:02 -0400

    callback is added in connection strng

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://admin:admin@cluster0.gxzxfor.mongodb.net/Kiosk?retryWrites=true&w=majority")
-
-
-const app = new express();
+const app = express();
 app.set('view engine', 'ejs');
 const port = 7000;
+
+mongoose.connect("mongodb+srv://admin:admin@cluster0.gxzxfor.mongodb.net/Kiosk?retryWrites=true&w=majority", (err, cnt) => {
+    if(err){
+        console.log(`Error while connecting to the database`, err);
+    }else{
+        console.log(`Connected to the database`);
+    }
+})
+
+
 
 app.use(express.static('public'));
 
